e59b9018f52c0fe273c667ffb146513ae339d717
Ngoc Huynh <mhuynh112@student.cccd.edu>
2025-06-04 01:22:46 -0700

    fix sidebar and add archive

--- reana/src/app/page.jsx
+++ reana/src/app/page.jsx
@@ -25,7 +25,7 @@ export default function WelcomePage() {
     }
 
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&libraries=places`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
     script.async = true;
     script.defer = true;
     
